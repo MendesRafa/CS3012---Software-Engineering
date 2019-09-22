fe23b6621ee168d5c5b7e0ea672a1aeae96c290a
MendesRafa <rafaman089@outlook.com>
2019-09-22 20:37:54 +0100

    Created testIsNodeValidWithTrivialGraph
    Created a test for the isNodeValid function for the case where the graph is trivial i.e. only has one node

--- src/LowestCommonAncestorTest.java
+++ src/LowestCommonAncestorTest.java
@@ -21,4 +21,17 @@ public class LowestCommonAncestorTest {
 		assertFalse(LowestCommonAncestor.isNodeValid(root, targetNode),
 				"If the graph is empty and the node is NOT null then the inputs are not valid");
 	}
+
+	@Test
+	public void tesIsNodeValidWithTrivialGraph() {
+		Node root = new Node(1);
+
+		Node targetNode = new Node(1);
+		assertTrue(LowestCommonAncestor.isNodeValid(root, targetNode),
+				"In the trivial graph if the targetNode is equal to the root it IS VALID");
+
+		targetNode = new Node(2);
+		assertFalse(LowestCommonAncestor.isNodeValid(root, targetNode),
+				"In the trivial graph if the targetNode is NOT equal to the root it IS NOT VALID");
+	}
 }
