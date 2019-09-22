4431e13d4e96f67b03deeac95243b8a0c92cfdec
MendesRafa <rafaman089@outlook.com>
2019-09-22 20:30:15 +0100

    Made testIsNodeValidWithEmptyGraph
    Created the isNodeValid function which always returns false if the root is null

---
+++ src/LowestCommonAncestor.java
@@ -0,0 +1,14 @@
+import java.util.function.BooleanSupplier;
+
+public class LowestCommonAncestor {
+
+	//checks if the LCA arguments are valid
+	public static boolean isNodeValid(Node root, Node node) {
+		if (root == null) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+}
