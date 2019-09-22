86ff087ddfdbe25a51e019a06b136986c5fdc722
MendesRafa <rafaman089@outlook.com>
2019-09-22 21:08:28 +0100

    Extended isNodeValid making tests pass
    Extended the isNodeValid function so that when the targetNode has the same value as the root it returns true

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -3,12 +3,14 @@ import java.util.function.BooleanSupplier;
 public class LowestCommonAncestor {
 
 	//checks if the LCA arguments are valid
-	public static boolean isNodeValid(Node root, Node node) {
+	public static boolean isNodeValid(Node root, Node targetNode) {
 		if (root == null) {
 			return false;
 		}
-		else {
+		if (root.value == targetNode.value) {
 			return true;
 		}
+		
+		return false;
 	}
 }
