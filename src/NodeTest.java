b0ddd66b00bad3e5d1a8be03199f14793255201b
MendesRafa <rafaman089@outlook.com>
2019-09-22 15:42:55 +0100

    Create Test to show how the Node Class can be used to make a Biary Tree Simple test added to show that a simple BT can be generated and by which means this could be achieved

--- src/NodeTest.java
+++ src/NodeTest.java
@@ -5,12 +5,21 @@ import org.junit.jupiter.api.Test;
 class NodeTest {
 
 	@Test
-	public void testConstructor()
-	{
+	public void testConstructor() {
 		Node testNode = new Node(5);
 		assertEquals(testNode.value, 5, "Check if the value of the generated node is correct");
 		assertNull(testNode.rightChild, "Check if the value of the right child node is correct");
 		assertNull(testNode.leftChild, "Check if the value of the left child node is correct");
+	}
+
+	@Test
+	public void testConstructingASimpleBST() {
+		Node root = new Node(1);
+		root.rightChild = new Node(2);
+		root.leftChild = new Node(3);
+		assertEquals(root.value, 1, "Check if the value of the generated node is correct");
+		assertEquals(root.rightChild.value, 2, "Check if the value of the right child node is correct");
+		assertEquals(root.leftChild.value, 3, "Check if the value of the left child node is correct");
 	}
 
 }
